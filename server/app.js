5eb89af4d2477921ed149854885b1f2a3a3aa2d7
Sujeet kushwaha <sujeetgroups@gmail.com>
2023-09-16 11:28:52 +0530

    add specific origin

--- server/app.js
+++ server/app.js
@@ -4,6 +4,19 @@ const cors = require('cors')
 
 const app = express()
 
+// Specify the allowed origins as an array of URLs
+const allowedOrigins = ['https://seo-analizer-tool-f7yy.vercel.app/', 'https://localhost:3000'];
+
+// Configure CORS to allow only the specified origins
+app.use(cors({
+  origin: allowedOrigins,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable credentials (cookies, authorization headers)
+}));
+
+
+
+
 app.use(cors())
 app.use(express.json())
 
